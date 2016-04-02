bc403dfbee9434ad30385b9a9777d23b268704a9
nataliya3110 <nataliya.hoynyak@gmail.com>
2016-04-02 13:09:46 +0000

    asacdfv

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// JavaScript File
+$('#btn-send').click(function() {
+  // Please set your email in url field
+  $.ajax({
+      url: "https://formspree.io/nataliya.hoynyak@gmail.com",  
+      method: "POST",
+      data: {
+             email: email.value,
+             message: msg.value
+             },
+      dataType: "json"
+  }).done(function() {
+     $('#container').html('<h1>Thank you!</h1>')
+  } );
+});
